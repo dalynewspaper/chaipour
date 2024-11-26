87caf2ae6edf5380fdae1568b7d494b6373121cb
Brian <dalynewspaper@gmail.com>
2024-11-27 00:16:06 +0100

    Initial commit for Chaipour React project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import teapot from "../assets/teapotBeige.svg";
 
 const Header = () => {
   return (
     <header>
+    <img src={teapot} alt="Teapot" style={{ width: "100px", marginBottom: "10px" }} />
       <h1>ChaiPour</h1>
       <h2>Speciality Indian Tea</h2>
       <p>विशेष भारतीय चाय</p>
